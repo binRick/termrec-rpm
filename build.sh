1968ff5c57eab9f2155e31c578aea844159ac704
root <admin@vpntech.net>
2019-06-21 13:25:39 -0400

    refactor script to use template

--- build.sh
+++ build.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-rm -rf termrec termrec-latest
-mkdir -p ~/rpmbuild/SOURCES
-rm -rf ~/rpmbuild/SOURCES/termrec-latest.tar.gz termrec-latest
+DIR_BASE=$(cd ${BASH_SOURCE[0]%/*} && pwd)
+RELEASE_VERSION=$1
+SPEC_FILE=termrec.spec
+if [ "$RELEASE_VERSION" == "" ]; then
+  echo "First argument must be release version to compile"; exit 1
+fi
+
 set -e
-git clone https://github.com/kilobyte/termrec termrec-latest
-tar -czf ~/rpmbuild/SOURCES/termrec-latest.tar.gz termrec-latest && \
-rpmbuild -bb termrec.spec
+
+if [ -f ".${SPEC_FILE}" ]; then
+  unlink .${SPEC_FILE}
+fi
+
+cp ${SPEC_FILE}.template .${SPEC_FILE}
+sed -i "s/__RELEASE_VERSION__/${RELEASE_VERSION}/g" .${SPEC_FILE}
+
+rm -rf termrec termrec-${RELEASE_VERSION}
+mkdir -p ~/rpmbuild/SOURCES
+rm -rf ~/rpmbuild/SOURCES/termrec-${RELEASE_VERSION} termrec-${RELEASE_VERSION}
+set -e
+
+wget https://github.com/kilobyte/termrec/archive/v${RELEASE_VERSION}.tar.gz -O termrec-${RELEASE_VERSION}.tar.gz
+mv termrec-${RELEASE_VERSION}.tar.gz ~/rpmbuild/SOURCES/
+
+rpmbuild -bb .${SPEC_FILE}
 set +e
 
-rm -rf termrec termrec-latest
+rm -rf termrec termrec-${RELEASE_VERSION}
